d3994901d3e6364110108fcb439d6b700ffd3b65
Sudhanshu0302dixit <121972536+Sudhanshu0302dixit@users.noreply.github.com>
2024-04-14 00:00:54 +0530

    Add files via upload

--- finalSudoku.cpp
+++ finalSudoku.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-#include <cstdlib> // for rand and srand
-#include <ctime>   // for time
-#include <windows.h> // for changing console colors
+#include <cstdlib> 
+#include <ctime>   
+#include <windows.h> 
+using namespace std;
 
-const int N = 9; // Size of the Sudoku board
+const int N = 9; 
 
-// Function to print the Sudoku board
+
 void printBoard(int board[N][N], bool highlight[N][N] = nullptr) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, 15); // Set text color to white
+    SetConsoleTextAttribute(hConsole, 19); 
 
-    std::cout << "\x1b[48;2;0;0;0m"; // Set background color to black using ANSI escape code
-
+    std::cout << "\x1b[48;2;0;0;0m"; 
     std::cout << "+---------+---------+---------+\n";
     for (int i = 0; i < N; ++i) {
         std::cout << "| ";
         for (int j = 0; j < N; ++j) {
             if (highlight && highlight[i][j]) {
-                SetConsoleTextAttribute(hConsole, 12); // Set text color to red for highlighted cells
+                SetConsoleTextAttribute(hConsole, 12); 
                 std::cout << board[i][j];
             } else {
                 if (board[i][j] == 0) {
@@ -37,10 +37,12 @@ void printBoard(int board[N][N], bool highlight[N][N] = nullptr) {
         }
     }
     std::cout << "+---------+---------+---------+\n";
-    SetConsoleTextAttribute(hConsole, 15); // Reset text color to white
+    system("color 03");
+
+    SetConsoleTextAttribute(hConsole, 15); 
 }
 
-// Function to generate a random solved Sudoku board
+
 void generateRandomSolvedSudokuBoard(int board[N][N]) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -49,48 +51,51 @@ void generateRandomSolvedSudokuBoard(int board[N][N]) {
     }
 }
 
-// Function to generate a random unsolved Sudoku board and modify it in place
+
 void generateRandomUnsolvedSudokuBoard(int board[N][N]) {
-    // Initialize the random number generator
+    
     std::srand(std::time(nullptr));
 
-    // Generate a random solved Sudoku board
+    
     int solvedBoard[N][N];
     generateRandomSolvedSudokuBoard(solvedBoard);
 
-    // Copy the solved board to the unsolved board
+    
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             board[i][j] = solvedBoard[i][j];
         }
     }
 
-    // Remove some numbers from the unsolved board to make it unsolved
+    
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            if (std::rand() % 2 == 0) { // Randomly remove some numbers
-                board[i][j] = 0; // 0 represents an empty cell
+            if (std::rand() % 2 == 0) { 
+                board[i][j] = 0; 
             }
         }
     }
 }
 
-// Function to provide instructions for playing Sudoku
+
 void playSudoku() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, 14); // Set text color to yellow
+    SetConsoleTextAttribute(hConsole, 14); 
 
-    std::cout << " Welcome to Sudoku!\n";
-    std::cout << " To play, enter the numbers 1-9 to fill in the empty cells.\n";
-    std::cout << " You can't change the pre-filled numbers.\n";
-    std::cout << " Try to fill each row, column, and 3x3 subgrid with the numbers 1-9 without repetition.\n";
-    std::cout << " Enter '0' for an empty cell.\n";
-    std::cout << " Good luck!\n\n";
+    std::cout << "\t\t\t<===============================================================================================>" << endl;
+    std::cout << "\t\t\t|                              WELCOME TO SUDOKU Game!                                          |" << endl;
+    std::cout << "\t\t\t|         Fill in the missing numbers(represented by dot(.)) to solve the puzzle.               |" << endl;
+    std::cout << "\t\t\t|              To play, enter the numbers 1-9 to fill in the empty cells.                       |" << endl;
+    std::cout << "\t\t\t|                    You can't change the pre-filled numbers.                                   |" << endl;
+    std::cout << "\t\t\t|    Try to fill each row, column, and 3x3 subgrid with the numbers 1-9 without repetition.     |" << endl;
+    std::cout << "\t\t\t|                           Enter '0' for an empty cell.                                        |" << endl;
+    std::cout << "\t\t\t|                                    Good luck!                                                 |"<< endl;
+    std::cout << "\t\t\t<===============================================================================================>" << endl;
 
-    SetConsoleTextAttribute(hConsole, 15); // Reset text color to white
+    SetConsoleTextAttribute(hConsole, 10); 
 }
 
-// Function to check the solution of the Sudoku board and return true if correct, false otherwise
+
 bool checkSolution(int board[N][N], int solution[N][N]) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -102,13 +107,13 @@ bool checkSolution(int board[N][N], int solution[N][N]) {
     return true;
 }
 
-// Function to let the user play Sudoku
+
 void playSudokuGame(int board[N][N], int solution[N][N]) {
     playSudoku();
     std::cout << " Here's the Sudoku board. Enter the numbers for each cell:\n";
     printBoard(board);
 
-    // User input for each cell
+    
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             if (board[i][j] == 0) {
@@ -120,50 +125,50 @@ void playSudokuGame(int board[N][N], int solution[N][N]) {
         }
     }
 
-    // Check the solution
+    
     if (checkSolution(board, solution)) {
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, 10); // Set text color to green
+        SetConsoleTextAttribute(hConsole, 10); 
         std::cout << "\n Congratulations! You've solved the Sudoku puzzle!\n";
-        SetConsoleTextAttribute(hConsole, 15); // Reset text color to white
+        SetConsoleTextAttribute(hConsole, 15); 
     } else {
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, 12); // Set text color to red
+        SetConsoleTextAttribute(hConsole, 12); 
         std::cout << "\n Sorry, the solution is incorrect. Better luck next time!\n";
-        SetConsoleTextAttribute(hConsole, 15); // Reset text color to white
+        SetConsoleTextAttribute(hConsole, 15); 
     }
 }
 
-// Function to print the solution of the Sudoku board
+
 void printSolution() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, 14); // Set text color to yellow
+    SetConsoleTextAttribute(hConsole, 14); 
 
     int solvedBoard[N][N];
     generateRandomSolvedSudokuBoard(solvedBoard);
     std::cout << "\n Here's the correct solution:\n";
     printBoard(solvedBoard);
 
-    SetConsoleTextAttribute(hConsole, 15); // Reset text color to white
+    SetConsoleTextAttribute(hConsole, 12); 
 }
 
 int main() {
     int sudokuBoard[N][N];
     int sudokuSolution[N][N];
 
-    // Generate a random unsolved Sudoku board
+    
     generateRandomUnsolvedSudokuBoard(sudokuBoard);
 
-    // Generate the corresponding solved Sudoku board
+    
     generateRandomSolvedSudokuBoard(sudokuSolution);
 
-    // Change the background color of the terminal using ANSI escape codes
-    std::cout << "\x1b[48;2;0;0;0m"; // Set background color to black
+   
+    // std::cout << "\033[31;1;4m"; 
 
-    // Provide instructions and let the user play Sudoku
+    
     playSudokuGame(sudokuBoard, sudokuSolution);
 
-    // Print the correct solution after the game ends
+    
     printSolution();
 
     return 0;
